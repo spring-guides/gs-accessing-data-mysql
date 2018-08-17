77ec5d1da3a4f078c047f57f4d1e8ad6ced98a74
twofor22 <twofor22@users.noreply.github.com>
2018-08-17 20:04:05 +1000

    Fix repository id type

--- complete/src/main/java/hello/UserRepository.java
+++ complete/src/main/java/hello/UserRepository.java
@@ -7,6 +7,6 @@ import hello.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
