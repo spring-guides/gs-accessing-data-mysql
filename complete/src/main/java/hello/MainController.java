c4b7d7bec807cae8abcb79ff5a32eb2cd5c48edc
Dave Syer <dsyer@pivotal.io>
2019-07-27 16:32:28 +0100

    Polish

--- complete/src/main/java/hello/MainController.java
+++ complete/src/main/java/hello/MainController.java
@@ -2,13 +2,11 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import hello.User;
-import hello.UserRepository;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
