b40f0032fa80855269a5f88def5401d2134c28b5
Jay Bryant <jbryant@pivotal.io>
2019-10-18 13:40:42 -0500

    Upgrade to Spring Boot 2.2
    I upgraded the build files to use Spring 2.2. Doing so necessitated upgrading the Gradle wrappers. Note that there are no tests in the /complete branch. I added the test from the Initializr in the /initial branch.

---
+++ initial/src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
