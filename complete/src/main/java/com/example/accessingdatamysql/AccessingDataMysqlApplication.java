7b07eff7f999a53d3f2d2ffd5b88ac9f15574d46
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:07:09 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AccessingDataMysqlApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+	}
 }
--- complete/src/main/java/com/example/accessingdatamysql/User.java
+++ complete/src/main/java/com/example/accessingdatamysql/User.java
@@ -7,13 +7,13 @@ import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 
-    private String name;
+	private String name;
 
-    private String email;
+	private String email;
 
 	public Integer getId() {
 		return id;
